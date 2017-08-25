af35c0a8b4fb5590bb89aa58b7ce2bc43481ea86
Saul Medrano <saulmedrano@carfax.com>
2017-08-25 05:11:00 -0400

    First commit

---
+++ src/main/groovy/com/me/testing/TestingApplication.groovy
@@ -0,0 +1,15 @@
+package com.me.testing
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TestingApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run TestingApplication, args
+
+
+
+	}
+}
