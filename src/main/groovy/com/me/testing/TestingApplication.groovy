a7af9dd9501ed81edcdca699e494be5f8d3378de
Saul Medrano <saulmedrano@carfax.com>
2017-08-27 17:27:49 -0400

    add annotation to exclude the datasource configuration

--- src/main/groovy/com/me/testing/TestingApplication.groovy
+++ src/main/groovy/com/me/testing/TestingApplication.groovy
@@ -1,9 +1,12 @@
 package com.me.testing
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude=DataSourceAutoConfiguration.class)
 class TestingApplication {
 
 	static void main(String[] args) {
