fecb326eb2b654cfd7f40c514e350cad461f892e
Saul Medrano <saulmedrano@carfax.com>
2017-08-27 17:30:28 -0400

    Add script for the properties H2 db

---
+++ src/main/resources/property.sql
@@ -0,0 +1,15 @@
+SET MODE = MySQL;
+
+create table property (
+  finca_raiz_id bigint not null auto_increment
+, property_type varchar(17)
+, price decimal(20, 2)
+, private_area int
+, built_area int
+, estrato int
+, address VARCHAR (30)
+, description VARCHAR (40000)
+, updated_date date
+, url VARCHAR (500)
+, PRIMARY KEY (finca_raiz_id)
+);
