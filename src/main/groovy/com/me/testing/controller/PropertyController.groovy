f1250f50e981eb276010f64129d46da5321932ed
Saul Medrano <saulmedrano@carfax.com>
2017-08-28 22:30:47 -0400

    change controller for a restController

--- src/main/groovy/com/me/testing/controller/PropertyController.groovy
+++ src/main/groovy/com/me/testing/controller/PropertyController.groovy
@@ -5,12 +5,12 @@ import com.me.testing.service.PropertyService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController("/")
 class PropertyController {
     PropertyService propertyService
     @Autowired
@@ -18,7 +18,7 @@ class PropertyController {
         this.propertyService = propertyService
 
     }
-    @RequestMapping(value='/propertiesbyestrato/{estrato}', method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value='/propertiesbyestrato/', method = RequestMethod.GET, produces = "application/json")
     def getPropertiesByEstrato(@RequestParam int estrato){
         try {
             List<Property> properties = propertyService.getPropertiesByEstrato(estrato)?:[]
